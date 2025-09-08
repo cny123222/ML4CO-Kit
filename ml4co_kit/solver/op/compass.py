6c01ef50a78a9a3bafc54ebbf36bf1a54149a596
cny123222 <cny123222@sjtu.edu.cn>
2025-09-08 23:40:33 +0800

    Complete compass solver for op

--- ml4co_kit/solver/op/compass.py
+++ ml4co_kit/solver/op/compass.py
@@ -12,10 +12,13 @@ Compass Solver for solving OP.
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
 
-import itertools
+import os
+import tempfile
+import shutil
 import numpy as np
 from multiprocessing import Pool
 from typing import Union, Tuple, List
+from subprocess import check_output
 from ml4co_kit.solver.op.base import OPSolver
 from ml4co_kit.utils.type_utils import SOLVER_TYPE
 from ml4co_kit.utils.time_utils import iterative_execution, Timer
@@ -24,20 +27,22 @@ from ml4co_kit.utils.time_utils import iterative_execution, Timer
 class OPCompassSolver(OPSolver):
     def __init__(
         self, 
-        scale: int = 1e6, 
-        time_limit: float = 120.0, 
-        gurobi_gap: float = 0.0,
+        scale: int = 1e7, 
         precision: Union[np.float32, np.float64] = np.float32
     ):
         super(OPCompassSolver, self).__init__(
-            solver_type=SOLVER_TYPE.GUROBI, scale=scale, precision=precision
+            solver_type=SOLVER_TYPE.COMPASS, scale=scale, precision=precision
         )
-        self.time_limit = time_limit
-        self.gurobi_gap = gurobi_gap
+        self.executable = shutil.which("compass")
+        assert self.executable is not None, (
+            "Could not find the 'compass' executable in the system's PATH.\n"
+            "Please ensure that Compass is installed correctly and that the directory "
+            "containing the 'compass' executable (e.g., /usr/local/bin) is in the PATH environment variable."
+        )
         
     def _solve(
         self, 
-        depots: np.ndarray, 
+        depot: np.ndarray, 
         points: np.ndarray, 
         prizes: np.ndarray, 
         max_length: float
@@ -45,6 +50,38 @@ class OPCompassSolver(OPSolver):
         """
         Solve a single OP instance using Compass
         """
+        with tempfile.TemporaryDirectory() as tempdir:
+            problem_filename = os.path.join(tempdir, "problem.oplib")
+            tour_filename = os.path.join(tempdir, "solution.tour")
+            log_filename = os.path.join(tempdir, "compass.log")
+            
+            self._write_oplib(problem_filename, depot, points, prizes, max_length)
+            
+            try:
+                # Prepare the command
+                cmd = [
+                    self.executable,
+                    '--op',
+                    '--op-ea4op',
+                    problem_filename,
+                    '-o',
+                    tour_filename
+                ]
+                # Solve the problem
+                with open(log_filename, 'w') as f:
+                    check_output(cmd, stderr=f, stdout=f)
+                # Read the solution
+                tour = self._read_oplib(tour_filename)
+                tour_length = self._calc_op_length(depot, points, tour)
+                if not tour_length <= max_length + 1e-5:
+                    print(f"Warning: Tour length {tour_length} exceeds max_length {max_length}")
+                return tour
+                
+            except Exception as e:
+                print(f"An exception occurred while solving an OP instance with Compass: {e}")
+                with open(log_filename, 'r') as f:
+                    print("Compass log:\n", f.read())
+                return []
 
     def solve(
         self, 
@@ -104,6 +141,66 @@ class OPCompassSolver(OPSolver):
         
         # return
         return self.tours
+    
+    def _write_oplib(self, filename, depot, loc, prize, max_length, name="problem"):
+        with open(filename, 'w') as f:
+            f.write("\n".join([
+                "{} : {}".format(k, v)
+                for k, v in (
+                    ("NAME", name),
+                    ("TYPE", "OP"),
+                    ("DIMENSION", len(loc) + 1),
+                    ("COST_LIMIT", int(max_length * 10000000 + 0.5)),
+                    ("EDGE_WEIGHT_TYPE", "EUC_2D"),
+                )
+            ]))
+            f.write("\n")
+            f.write("NODE_COORD_SECTION\n")
+            f.write("\n".join([
+                "{}\t{}\t{}".format(i + 1, int(x * 10000000 + 0.5), int(y * 10000000 + 0.5))  # oplib does not take floats
+                #"{}\t{}\t{}".format(i + 1, x, y)
+                for i, (x, y) in enumerate([depot] + loc)
+            ]))
+            f.write("\n")
+            f.write("NODE_SCORE_SECTION\n")
+            f.write("\n".join([
+                "{}\t{}".format(i + 1, d)
+                for i, d in enumerate([0] + prize)
+            ]))
+            f.write("\n")
+            f.write("DEPOT_SECTION\n")
+            f.write("1\n")
+            f.write("-1\n")
+            f.write("EOF\n")
+            
+    def _read_oplib(self, filename, n):
+        with open(filename, 'r') as f:
+            tour = []
+            dimension = 0
+            started = False
+            for line in f:
+                if started:
+                    loc = int(line)
+                    if loc == -1:
+                        break
+                    tour.append(loc)
+                if line.startswith("DIMENSION"):
+                    dimension = int(line.split(" ")[-1])
 
+                if line.startswith("NODE_SEQUENCE_SECTION"):
+                    started = True
+        
+        assert len(tour) > 0, "Unexpected length"
+        tour = np.array(tour).astype(int) - 1  # Subtract 1 as depot is 1 and should be 0
+        assert tour[0] == 0  # Tour should start with depot
+        assert tour[-1] != 0  # Tour should not end with depot
+        return tour[1:].tolist()
+    
+    def _calc_op_length(self, depot, loc, tour):
+        assert len(np.unique(tour)) == len(tour), "Tour cannot contain duplicates"
+        loc_with_depot = np.vstack((np.array(depot)[None, :], np.array(loc)))
+        sorted_locs = loc_with_depot[np.concatenate(([0], tour, [0]))]
+        return np.linalg.norm(sorted_locs[1:] - sorted_locs[:-1], axis=-1).sum()
+            
     def __str__(self) -> str:
         return "OPGurobiSolver"
